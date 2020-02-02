4c000b4c6110c01a0fbf2e426949cf455ebaf2ea
Ivan <sluvkann@polytechnic.poltava.ua>
2020-02-02 17:51:13 +0200

    Add key to Listeners model

--- StudyingApp/Models/Listeners.cs
+++ StudyingApp/Models/Listeners.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace StudyingApp.Models
 {
     public class Listeners
     {
+        [Key]
         public int ListenerId { get; set; }
 
         public int StudentId { get; set; }
