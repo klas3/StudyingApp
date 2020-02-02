46136d814db795e9163651389764f5756a40481e
Ivan <i.i.nazarenko02@gmail.com>
2020-02-02 13:42:06 +0200

    Add models
    Add modes User, Module, enum Role

---
+++ StudyingApp/Models/Module.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace StudyingApp.Models
+{
+    public class Module
+    { 
+        [Key]
+        public int ModuleId { get; set; }
+        public Course Course { get; set; }
+        public string ModuleName { get; set; }
+        public bool IsTest { get; set; }
+        public bool IsLab { get; set; }
+
+    }
+}
---
+++ StudyingApp/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudyingApp.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public Role? UserRole { get; set; }
+    }
+}
