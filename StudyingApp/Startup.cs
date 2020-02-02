f3cd8df65f9119b1e72f9b62b76831b98da301fb
Ivan <sluvkann@polytechnic.poltava.ua>
2020-02-02 21:23:06 +0200

    Connect identity using

--- StudyingApp/Startup.cs
+++ StudyingApp/Startup.cs
@@ -1,14 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using StudyingApp.Data;
+using StudyingApp.Models;
 using StudyingApp.Repositories;
 
 namespace StudyingApp
@@ -25,12 +28,25 @@ namespace StudyingApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentity<User, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 7;
+                options.Password.RequireUppercase = true;
+            })
+            .AddEntityFrameworkStores<StudiyingAppContext>();
+
             services.AddTransient<IRepository, Repository>();
 
             services.AddControllersWithViews();
 
             services.AddDbContext<StudiyingAppContext>(options =>
                 options.UseSqlite("Data Source=cupcake.db"));
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("RequireEmail", policy => policy.RequireClaim(ClaimTypes.Email));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
