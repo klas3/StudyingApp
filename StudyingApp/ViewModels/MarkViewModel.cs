29d704f83f4b5181c3dbc9740b837297dfd7c31e
Arsen <logus3900@gmail.com>
2020-02-08 19:47:37 +0200

    Add Module with roles

--- StudyingApp/ViewModels/MarkViewModel.cs
+++ StudyingApp/ViewModels/MarkViewModel.cs
@@ -7,24 +7,13 @@ namespace StudyingApp.ViewModels
 {
     public class MarkViewModel
     {
-        [Display(Name = "Модуль")]
-        [Required(ErrorMessage = "Оберіть модуль")]
-        public virtual Module Modules { get; set; }
-       
-
         [Display(Name = "Студент")]
         [Required(ErrorMessage = "Оберіть студента")]
         public virtual Listeners Listeners { get; set; }
 
 
-        [Display(Name = "Оцінка за лабораторну")]
-        [Range(0, 100)]
-        [Required(ErrorMessage = "Внесіть оцінку в діапазоні від 0 до 100")]
-        public int LabMark { get; set; }
+        [Display(Name = "Оцінка")]
+        public int Mark { get; set; }
 
-        [Display(Name = "Оцінка за тест")]
-        [Range(0, 25)]
-        [Required(ErrorMessage = "Внесіть оцінку в діапазоні від 0 до 25")]
-        public int TestMark { get; set; }
     }
 }
