9f0ad11bace9ed5e8f6032a491c430b82422705b
Arsen <logus3900@gmail.com>
2020-02-08 17:18:58 +0200

    CourseViewModel add

---
+++ StudyingApp/ViewModels/CourseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyingApp.ViewModels
+{
+    public class CourseViewModel
+    {
+        public string Name { get; set; }
+
+        public int Year { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+    }
+}
